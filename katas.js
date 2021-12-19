f2a8582ad912bcd58c79dffbb3261fe1c0c8257b
Marco Muser <marco.muser@posteo.de>
2021-12-19 17:17:16 +0100

    add descendingOrder

--- katas.js
+++ katas.js
@@ -9,3 +9,15 @@ const b = [1, 2, "aasf", "1", "123", 123];
 const filter_list = (list) => {
   return list.filter((el) => Number.isInteger(el));
 };
+
+const c = 1021;
+const descendingOrder = (n) => {
+  return Number(
+    Array.from(n.toString())
+      .map(Number)
+      .sort((a, z) => z - a)
+      .join("")
+  );
+};
+
+console.log(descendingOrder(c));
